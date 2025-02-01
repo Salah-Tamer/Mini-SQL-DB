2a75479d87aa253732ca05bac8333e1ac35eb208
Salah Tamer <ma9060139@gmail.com>
2025-02-01 19:17:58 +0200

    feat: added indexing.js

--- test/test_db.js
+++ test/test_db.js
@@ -1,5 +1,6 @@
 // test/test_db.js
 
+import { createIndex, searchWithIndex } from "../lib/indexing.js";
 import { insertInto, select } from "../lib/query.js";
 import { createTable } from "../lib/schema.js";
 import { logger } from "../logger/logger.js";
@@ -21,7 +22,7 @@ function testInsertInto_v1() {
   const insertQuery_3 = "INSERT INTO users (id, name, age, student) VALUES (3, 'Adham', 22, true)";
   const insertQuery_4 = "INSERT INTO users (id, name, age, student) VALUES (4, 'Omar', 25, false)";
   const insertQuery_5 =
-    "INSERT INTO users (id, name, age, student) VALUES (4, 'Youssef', 14, true)";
+    "INSERT INTO users (id, name, age, student) VALUES (4, 'Youssef', 19, true)";
   try {
     insertInto(insertQuery_1);
     insertInto(insertQuery_2);
@@ -48,10 +49,32 @@ function testSelect_v1() {
   }
 }
 
+function testCreateIndex_v1() {
+  try {
+    createIndex("users", "name");
+    createIndex("users", "age");
+    logger("TEST", pc.magenta, console.error, "Create Index test passed\n");
+  } catch (error) {
+    logger("TEST", pc.red, console.error, "Create Index test failed\n", error);
+  }
+}
+
+function testSearchWithIndex_v1() {
+  try {
+    searchWithIndex("users", "name", "Salah");
+    searchWithIndex("users", "age", 19);
+    logger("TEST", pc.magenta, console.error, "Search Index test passed\n");
+  } catch (error) {
+    logger("TEST", pc.red, console.error, "Search Index test failed\n", error);
+  }
+}
+
 function main() {
   testCreateTable_v1();
   testInsertInto_v1();
   testSelect_v1();
+  testCreateIndex_v1();
+  testSearchWithIndex_v1();
 }
 
 main();
