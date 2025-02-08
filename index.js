9aebc192c8e0ad903ef3707223782c985a4e920b
Salah Tamer <ma9060139@gmail.com>
2025-02-08 21:34:57 +0200

    feat: update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // index.js
 
-import { logError, logDebug, logInfo, logSuccess } from "./logger/logger.js";
+//Storage
+export { DATABASE_PATH, getTableFilePath, readJSON, writeJSON } from "./lib/storage.js";
 
-logError("This my error");
-logDebug("My debug");
-logInfo("My Info");
-logSuccess("My Success");
+//Schema
+export { createTable } from "./lib/schema.js";
+
+//Queries
+export { insertInto, select } from "./lib/query.js";
+
+//Indexing
+export { createIndex, searchWithIndex } from "./lib/indexing.js";
+
+//Backup
+export { backupDatabase, restoreDatabase } from "./lib/backup.js";
+
+//Lock
+export { lockTable, performWithLock } from "./lib/lock.js";
+
+//log
+export { logError, logDebug, logInfo, logSuccess } from "./logger/logger.js";
